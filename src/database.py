ff6fb2e95abbf1e35da8779f0138af6ca6070416
Serhii Chukin <serhiichukin@gmil.com>
2025-11-05 15:27:07 +0100

    add base structure

---
+++ src/database.py
@@ -0,0 +1,29 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy import create_engine, Engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Session
+
+from . import config
+
+engine: Engine = create_engine(
+    config.DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal: sessionmaker[Session] = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine
+)
+
+
+def get_db():
+    db: Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+class Base(DeclarativeBase): ...
